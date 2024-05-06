81aa57d0e792d836a45bfe713f56dc60b50b8327
Steven <spatrick96@posteo.net>
2024-05-06 14:50:06 -0700

    Added comments to the code to make it more readable

--- project_main.py
+++ project_main.py
@@ -19,24 +19,28 @@ from PySide6.QtCore import Qt
 class Window(QMainWindow):
 
     def __init__(self):
-
         super().__init__(parent=None)
-
+        #This is how to dynamically add more tools. Simply just added the tool name as the key and the function name as the data and it will be added
         self.dict_of_menu_options = {}
         self.dict_of_toolbar_options={}
+
+        #The image goes here
         self.image_label = QLabel("No Image Loaded")
+        #This is for the scaling of an image
         self.scale_factor = 1.0
 
-        self.setWindowTitle("QMainWindow")
-
-        self.load_image()
+        self.setWindowTitle("Basic Image Manipulator")
+        #Uncomment if you want to load an image at startup
+        # self.load_image()
+        #
         self.add_load_image_to_menu()
-
         self.create_zoom_buttons()
 
-        self.create_menuBar()
-
+        #Add new tools must be functions must be added before these functions. These functions create the menus
+        self.createMenuBar()
         self.createToolBar()
+
+        #Can change this to tell you which tool you have selected? Currently does nothing important
         self._createStatusBar()
 
         # Create a central widget and set the layout on it
@@ -44,18 +48,21 @@ class Window(QMainWindow):
         self.central_layout = QVBoxLayout()
         self.central_layout.addWidget(self.image_label)
         self.central_widget.setLayout(self.central_layout)
-        self.setCentralWidget(self.central_widget)
-
-    def populate_menu(self):
+        self.setCentralWidget(self.central_widget)     
+        
+    def createMenuBar(self):
+        """
+        Loops through the dictionary of menu options and adds them to the main menu.
+        """
         menu = self.menuBar().addMenu("&Menu")
         self.create_menu_tool("&Exit", self.close)
         for item, function in self.dict_of_menu_options.items():
             menu.addAction(item, function)
-        
-    def create_menuBar(self):
-        self.populate_menu()
 
     def createToolBar(self):
+        """
+        Loops through the dictionary of toolbar options and adds them to the toolbar.
+        """
         tools = QToolBar()
         self.create_toolbar_tool("Exit", self.close)
         for item, function in self.dict_of_toolbar_options.items():
@@ -63,22 +70,37 @@ class Window(QMainWindow):
         self.addToolBar(tools)
 
     def _createStatusBar(self):
+        """
+        Currently does nothing.
+        """
         status = QStatusBar()
         status.showMessage("I'm the Status Bar")
         self.setStatusBar(status)
 
     def create_menu_tool(self, tool, function):
+        """
+        Helper function to just add a tool to the menu
+        """
         self.dict_of_menu_options[tool] = function
 
     def create_toolbar_tool(self, tool, function):
+        """
+        Helper function to just add a tool to the toolbar
+        """
         self.dict_of_toolbar_options[tool] = function
 
     def add_load_image_to_menu(self):
+        """
+        Adds load image to the menu
+        """
         self.create_menu_tool("&Load_Image", self.load_image)
 
     def load_image(self):
+        """
+        This loads images with a certain file name tag. This is to prevent crashing the program.
+        """
         file_dialog = QFileDialog(self)
-        file_dialog.setNameFilter("Images (*.png *.jpg *.bmp)")
+        file_dialog.setNameFilter("Images (*.png *.jpg *.JPG *.bmp)")
         file_dialog.setViewMode(QFileDialog.Detail)
         file_dialog.setFileMode(QFileDialog.ExistingFile)
 
@@ -92,27 +114,34 @@ class Window(QMainWindow):
                 self.image_label.setText("Invalid Image")
 
     def update_image(self):
+        """
+        This function currently just updates the image based on just the zoom in and out. Can be changed to be more of a general function
+        """
         scaled_width = int(self.original_pixmap.width() * self.scale_factor)
         #scaled_height = int(self.original_pixmap.height() * self.scale_factor)
         scaled_pixmap = self.original_pixmap.scaledToWidth(scaled_width)
         self.image_label.setPixmap(scaled_pixmap)
 
     def zoom_in(self):
+        """
+        Zooms in on an image
+        """
         self.scale_factor *= 1.1
         self.update_image()
 
     def zoom_out(self):
+        """
+        Zooms out on an image
+        """
         self.scale_factor *= 0.9
         self.update_image()
     
     
     def create_zoom_buttons(self):
-        zoom_in_button = QPushButton("Zoom In")
-        zoom_in_button.clicked.connect(self.zoom_in)
+        """
+        Adds the zooming functionality to the toolbar
+        """
         self.dict_of_toolbar_options["Zoom In"] = self.zoom_in
-        
-        zoom_out_button = QPushButton("Zoom Out")
-        zoom_out_button.clicked.connect(self.zoom_out)
         self.dict_of_toolbar_options["Zoom Out"] = self.zoom_out
 
 
