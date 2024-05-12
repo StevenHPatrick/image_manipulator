6e79f1b0cc19c98b71de6d8ace03e1517c92c4b6
Steven <spatrick96@posteo.net>
2024-05-12 00:29:42 -0700

    Fixed issues brought in from the branch merge and renabled cropping

--- project_main.py
+++ project_main.py
@@ -1,4 +1,5 @@
 import sys
+
 from PySide6.QtWidgets import (
     QApplication, QLabel, QMainWindow, QWidget, QStatusBar, QToolBar, QFileDialog,
     QVBoxLayout, QHBoxLayout, QPushButton
@@ -6,28 +7,41 @@ from PySide6.QtWidgets import (
 from PySide6.QtGui import QPixmap, QPainter, QPen, QColor
 from PySide6.QtCore import Qt, QRect
 
+
 class Window(QMainWindow):
+
     def __init__(self):
         super().__init__(parent=None)
+        #This is how to dynamically add more tools. Simply just added the tool name as the key and the function name as the data and it will be added
         self.dict_of_menu_options = {}
-        self.dict_of_toolbar_options = {}
-
-      
-        self.image_label = QLabel("No Image Loaded")
-        self.scale_factor = 1.0
-        self.original_pixmap = None
-        self.loaded_pixmap = None  
+        self.dict_of_toolbar_options={}
+        self.pixmap = None
+        self.loaded_pixmap = None
         self.crop_start = None
         self.crop_end = None
-        self.temp_pixmap = None
 
-      
+        #The image goes here
+        self.image_label = QLabel("No Image Loaded")
+        #This is for the scaling of an image
+        self.scale_factor = 1.0
+
         self.setWindowTitle("Basic Image Manipulator")
-        self.setup_toolbar()
-        self.setup_menu_bar()
-        self.setup_status_bar()
+        #Uncomment if you want to load an image at startup
+        # self.load_image()
+        #
+        self.open_image_button()
+        self.add_load_image_to_menu()
+        self.create_zoom_buttons()
+        self.create_crop_tools()
 
-        
+        #Add new tools must be functions must be added before these functions. These functions create the menus
+        self.createMenuBar()
+        self.createToolBar()
+
+        #Can change this to tell you which tool you have selected? Currently does nothing important
+        self.createStatusBar()
+
+        # Create a central widget and set the layout on it
         self.central_widget = QWidget()
         self.central_layout = QVBoxLayout()
         self.central_layout.addWidget(self.image_label)
@@ -38,75 +52,149 @@ class Window(QMainWindow):
         self.image_label.setMouseTracking(True)
         self.image_label.mousePressEvent = self.start_crop
         self.image_label.mouseMoveEvent = self.drawing_crop
-        self.image_label.mouseReleaseEvent = self.perform_crop
-
-    def setup_toolbar(self):
-        toolbar = QToolBar()
-        self.add_toolbar_button("Open Image", self.load_image, toolbar)
-        self.add_toolbar_button("Zoom In", self.zoom_in, toolbar)
-        self.add_toolbar_button("Zoom Out", self.zoom_out, toolbar)
-        self.add_toolbar_button("Crop", self.perform_crop, toolbar)
-        self.add_toolbar_button("Revert", self.revert_changes, toolbar)
-        self.addToolBar(toolbar)
-
-    def add_toolbar_button(self, label, function, toolbar):
-        button = QPushButton(label)
-        button.clicked.connect(function)
-        toolbar.addWidget(button)
-
-    def setup_menu_bar(self):
+        self.image_label.mouseReleaseEvent = self.perform_crop    
+        
+    def createMenuBar(self):
+        """
+        Loops through the dictionary of menu options and adds them to the main menu.
+        """
         menu = self.menuBar().addMenu("&Menu")
-        menu.addAction("&Exit", self.close)
+        self.create_menu_tool("&Exit", self.close)
+        for item, function in self.dict_of_menu_options.items():
+            menu.addAction(item, function)
 
-    def setup_status_bar(self):
-        self.status_bar = QStatusBar()
-        self.setStatusBar(self.status_bar)
-        self.status_bar.showMessage("Ready")
+    def createToolBar(self):
+        """
+        Loops through the dictionary of toolbar options and adds them to the toolbar.
+        """
+        tools = QToolBar()
+        self.create_toolbar_tool("Exit", self.close)
+        for item, function in self.dict_of_toolbar_options.items():
+            tools.addAction(item, function)
+        self.addToolBar(tools)
+
+    def createStatusBar(self):
+        """
+        Creates the status bar
+        """
+        self.status = QStatusBar()
+        self.update_status("Hello!")
+        
+
+    def update_status(self, message):
+        """
+        A helper function to update the status bar for various functions
+        """
+        self.status.showMessage(message)
+        self.setStatusBar(self.status)
+
+    def create_menu_tool(self, tool, function):
+        """
+        Helper function to just add a tool to the menu
+        """
+        self.dict_of_menu_options[tool] = function
+
+    def create_toolbar_tool(self, tool, function):
+        """
+        Helper function to just add a tool to the toolbar
+        """
+        self.dict_of_toolbar_options[tool] = function
+
+    def add_load_image_to_menu(self):
+        """
+        Adds load image to the menu
+        """
+        self.create_menu_tool("&Load_Image", self.load_image)
 
     def load_image(self):
+        """
+        This loads images with a certain file name tag. This is to prevent crashing the program.
+        """
         file_dialog = QFileDialog(self)
         file_dialog.setNameFilter("Images (*.png *.jpg *.JPG *.bmp)")
         file_dialog.setViewMode(QFileDialog.Detail)
         file_dialog.setFileMode(QFileDialog.ExistingFile)
+
         if file_dialog.exec():
             selected_file = file_dialog.selectedFiles()[0]
             pixmap = QPixmap(selected_file)
             if not pixmap.isNull():
-                self.original_pixmap = pixmap
-                self.loaded_pixmap = pixmap.copy()  # Store the original loaded image for reverting
+                self.pixmap = pixmap
+                self.loaded_pixmap = pixmap.copy() # Saving a copy for reverting
                 self.update_image()
-                self.status_bar.showMessage("Opened " + selected_file)
+                self.update_status("Opened " + selected_file)
             else:
                 self.image_label.setText("Invalid Image")
 
     def update_image(self):
-        if self.original_pixmap:
-            scaled_width = int(self.original_pixmap.width() * self.scale_factor)
-            scaled_pixmap = self.original_pixmap.scaledToWidth(scaled_width)
-            self.image_label.setPixmap(scaled_pixmap)
+        """
+        This function currently just updates the image based on just the zoom in and out. Can be changed to be more of a general function
+        """
+        scaled_width = int(self.pixmap.width() * self.scale_factor)
+        #scaled_height = int(self.pixmap.height() * self.scale_factor)
+        scaled_pixmap = self.pixmap.scaledToWidth(scaled_width)
+        self.image_label.setPixmap(scaled_pixmap)
 
     def zoom_in(self):
+        """
+        Zooms in on an image
+        """
         self.scale_factor *= 1.1
         self.update_image()
+        self.update_status("Zoomed In")
 
     def zoom_out(self):
+        """
+        Zooms out on an image
+        """
         self.scale_factor *= 0.9
         self.update_image()
+        self.update_status("Zoomed Out")
 
-    def revert_changes(self):
+    
+    
+    def create_zoom_buttons(self):
+        """
+        Adds the zooming functionality to the toolbar
+        """
+        self.dict_of_toolbar_options["Zoom In"] = self.zoom_in
+        self.dict_of_toolbar_options["Zoom Out"] = self.zoom_out
+
+    def open_image_button(self):
+        self.dict_of_toolbar_options["Open image"] = self.load_image
+
+    def crop_image(self):
+        """
+        Crop the image based on user selection.
+        """
+        if self.pixmap and self.crop_start and self.crop_end:
+            rect = QRect(self.crop_start, self.crop_end)
+            cropped_pixmap = self.pixmap.copy(rect)
+            self.pixmap = cropped_pixmap
+            self.update_image()
+            self.crop_start = None
+            self.crop_end = None
+            self.update_status("Crop applied.")
+
+
+    def revert(self):
+        """
+        Revert to the original image.
+        """
         if self.loaded_pixmap:
-            self.original_pixmap = self.loaded_pixmap.copy()
+            self.pixmap = self.loaded_pixmap.copy()
             self.scale_factor = 1.0  # Reset the scale factor
             self.update_image()
-            self.status_bar.showMessage("Changes reverted.")
+            self.update_status("Changes reverted.")
 
     def start_crop(self, event):
-        if self.original_pixmap:
+        if self.pixmap:
             self.crop_start = event.pos()
+            self.update_status("Crop Started")
 
     def drawing_crop(self, event):
-        if self.original_pixmap and self.crop_start:
-            self.temp_pixmap = self.original_pixmap.copy()
+        if self.pixmap and self.crop_start:
+            self.temp_pixmap = self.pixmap.copy()
             painter = QPainter(self.temp_pixmap)
             pen = QPen(QColor(255, 0, 0), 2, Qt.DashLine)
             painter.setPen(pen)
@@ -116,17 +204,22 @@ class Window(QMainWindow):
             self.crop_end = event.pos()
 
     def perform_crop(self, event):
-        if self.original_pixmap and self.crop_start and self.crop_end:
+        if self.pixmap and self.crop_start and self.crop_end:
             rect = QRect(self.crop_start, self.crop_end)
-            cropped_pixmap = self.original_pixmap.copy(rect)
-            self.original_pixmap = cropped_pixmap
+            cropped_pixmap = self.pixmap.copy(rect)
+            self.pixmap = cropped_pixmap
             self.update_image()
             self.crop_start = None
             self.crop_end = None
-            self.status_bar.showMessage("Crop applied.")
+            self.update_status("Crop applied.")
+
+    def create_crop_tools(self):
+        self.create_toolbar_tool("Crop", self.crop_image)
+        self.create_toolbar_tool("Revert", self.revert)
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     window = Window()
     window.show()
     sys.exit(app.exec())
